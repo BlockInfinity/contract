c32825de9657d54810f479cc869c75cff2983038
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-10-26 20:25:17 +0200

    Added bid and ask submitting, added flexible bid matching - not tested

--- contracts/Etherex.sol
+++ contracts/Etherex.sol
@@ -15,8 +15,8 @@ contract Etherex {
     struct Match {
       uint256 volume;
       uint256 price;
-      Order ask;
-      Order bid;
+      address askOwner;
+      address bidOwner;
     }
     
     //state times in minutes
@@ -26,21 +26,30 @@ contract Etherex {
 
     enum OrderType {Ask, Bid}
     struct Order {
+                
+        uint256 id;
+        uint256 nex;
         address owner;
         uint256 volume;
         uint256 price;
-        //Ask or bid order
-        OrderType typ;
+        
     }
+    mapping(uint256 => Order) idToOrder;
     
     uint8 public currState;
+    uint256 idCounter;
     
     mapping (address => address) userToSmartMeter;
     
-    Order bestAsk;
-    Order bestBid;
-    Match [] matches;
+    Order minAsk;
+    Order minBid;
+    Order [] flexBids;
+    uint256 flexBidVolume = 0;
+    //Additional array for flex bids, more optimal
+    
+     
 
+    Match [] matches;
 
     mapping (address => bool) certificateAuthorities;
     mapping (address => bool) public smartmeters;
@@ -49,6 +58,22 @@ contract Etherex {
     //if the balance is below 0, then send event that turns of energy
     mapping (address => uint256) public collateral;
 
+    //Linked list helper functions
+    //Returns next in list
+    function n(Order _o) internal returns(Order) {
+        return idToOrder[_o.nex];
+    }
+    //Binds the node into list
+    function bind(Order _prev, Order _curr, Order _next) internal{
+        _prev.nex = _curr.id;
+        _curr.id = _next.id;
+    }
+    
+    
+    function remove(Order _prev, Order _curr) internal{
+        _prev.nex = n(_curr).id;
+        delete _curr;
+    } 
 
     // modifiers
     modifier onlySmartMeters(){
@@ -64,42 +89,123 @@ contract Etherex {
         _;
     }
     
-    modifier onlyInState(uint8 state) {
-        if(state != currState) throw;
+    modifier onlyInState(uint8 _state) {
+        if(_state != currState) throw;
         _;
     }
     
-    //TODO Function that has to update the state based on time
+    //TODO Function that has to update the state based on time,
+    //should be called as often as possible :)
     function updateState() internal {
             
     }
     
     // Register Functions
-    function registerSmartmeter(address sm) onlyCertificateAuthorities(){
-      smartmeters[sm] = true;
+    function registerSmartmeter(address _sm) onlyCertificateAuthorities(){
+      smartmeters[_sm] = true;
     }
 
 
-    function submitBestBid(uint256 _volume) onlyInState(0) onlyUsers(){
-      
+    //If this is called, just put it on the beginning on the list
+    function submitFlexBid(uint256 _volume) onlyInState(0) onlyUsers(){
+        Order bid;
+        bid.volume = _volume;
+        flexBidVolume +=_volume;
+        flexBids.push(bid);
     }
 
-    function submitBid(uint256 _price, uint256 _amount) onlyInState(0) onlyUsers(){
-      
+    function submitBid(uint256 _price, uint256 _volume) onlyInState(0) onlyUsers(){
+        
+        Order bid;
+        bid.volume = _volume;
+        bid.id = idCounter++;
+        bid.price = _price;
+        bid.owner = msg.sender;
+        
+        //Iterate over list till same price encountered
+        Order memory curr = minBid;
+        Order memory prev;
+        prev.nex = minBid.id;
+        while(bid.price > curr.price) {
+            curr=n(curr);
+            prev = n(prev);
+        }
+        bind(prev, bid, curr);
+        
     }
-
-    function submitAsk(uint256 _price, uint256 _amount) onlyInState(0) onlyUsers(){
-
+    
+    //Calculate min ask to satisfy flexible bids on the way?
+    function submitAsk(uint256 _price, uint256 _volume) onlyInState(0) onlyUsers(){
+        
+        Order ask;
+        ask.volume = _volume;
+        ask.id = idCounter++;
+        ask.price = _price;
+        ask.owner = msg.sender;
+        
+        
+        
+        //Iterate over list till same price encountered
+        Order memory curr = minBid;
+        Order memory prev;
+        prev.nex = minAsk.id;
+        while(ask.price > curr.price) {
+            curr=n(curr);
+            prev = n(prev);
+        }
+        bind(prev, ask, curr);
+        
     } 
-
-    function submitCompAsk(uint256 _price, uint256 _amount) onlyInState(0) onlyUsers(){
+    
+    //TODO ?
+    function submitCompAsk(uint256 _price, uint256 _volume) onlyInState(0) onlyUsers(){
 
     } 
 
 
     //TODO Magnus Has to be automatically called from the blockchain
+    //Currently without accumulating, does accumulating make sense?
     function matching() onlyInState(1){
-
+        
+        Order prevBid;
+        Order prevAsk;
+        Order currBid = minBid;
+        Order currAsk = minAsk;
+        
+        //Solve flexible bids first
+        uint256 askVolume = 0;
+        uint256 price = 0;
+        while(askVolume < flexBidVolume) {
+            askVolume += currAsk.volume;
+            price = currAsk.price;
+        }
+        currAsk = minAsk;
+        //Wouldnt it be fair that all of them go to the aftermarket
+        //instead of only the last one? Round-robin too much?
+        for(uint i = 0; i < flexBids.length; i++ ) {
+            //TODO Create matches
+            if(currAsk.volume > flexBids[i].volume) {
+                matches.push(Match(flexBids[i].volume, price, currAsk.owner, flexBids[i].owner));
+                currAsk.volume -= flexBids[i].volume;
+            }else if(currAsk.volume < flexBids[i].volume) {
+                matches.push(Match(currAsk.volume, price, currAsk.owner, flexBids[i].owner));
+                flexBids[i].volume -= currAsk.volume;
+                uint tmp = currAsk.nex;
+                remove(prevAsk, currAsk);
+                currAsk = idToOrder[tmp]; 
+                i-=1;
+            } else {
+                matches.push(Match(currAsk.volume, price, currAsk.owner, flexBids[i].owner));
+                uint tmp2 = currAsk.nex;
+                remove(prevAsk, currAsk);
+                currAsk = idToOrder[tmp2]; 
+            }
+        }
+        //Iterate till you come to the end of ask or bid lists
+        while(currAsk.id != 0 && currBid.id != 0) {
+            //TODO create matches
+        }
+        
     }
 
     //TODO Magnus
@@ -112,22 +218,11 @@ contract Etherex {
 
     }
 
-
-    
-  
-    function addOrder() internal{
-
-    }
-
-    function removeOrder() internal{
-
-    } 
-
     //Constructor
   function Etherex(address _certificateAuthority) {
 
     certificateAuthorities[_certificateAuthority] = true;
-    
+    idCounter = 1;
 
   }
   
