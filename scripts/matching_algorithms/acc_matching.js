5c0bfdf397dee6a1a2d2817b99af0799a49adeef
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-10-27 17:31:23 +0200

    Added matching simulation in javascript, TODO: specify a deterministic behaviour of the algorithm (examples to use for testing)

---
+++ scripts/matching_algorithms/acc_matching.js
@@ -0,0 +1,176 @@
+/*
+
+Global TODOS:
+
+Modifiers for time dependant operations.
+Communication with smart meter.
+The order of the functions has to be determined.
+
+*/
+
+ 
+function n(bid) {
+    return bid.nex;
+}
+
+function bind(prev, curr, next) {
+    prev.nex = curr;
+    curr.nex = next;
+}
+
+//If this is called, just put it on the beginning on the list
+function submitFlexBid(_volume, _sender) {
+    var bid = {};
+    bid.volume = _volume;
+    bid.owner = _sender;
+    this.flexBidVolume +=_volume;
+    this.flexBids.push(bid);
+    console.log("Submitted flex bid volume(" + _volume + ") sender(" + _sender +")");
+}
+
+function submitBid(_price, _volume, _sender) {
+    
+    var bid = {};
+    bid.volume = _volume;
+    bid.id = this.idCounter++;
+    bid.price = _price;
+    bid.owner = _sender;
+
+    console.log("Submitted bid: " + bid.id);
+
+    if(this.minBid.id == 0) {
+        this.minBid = bid;
+        return;
+    }
+    
+    //Iterate over list till same price encountered
+    var curr = this.minBid;
+    var prev = {};
+    prev.nex = this.minBid;
+    while(curr && bid.price > curr.price) {
+        curr=n(curr);
+        prev = n(prev);
+    }
+    bind(prev, bid, curr);
+    
+}
+
+//Calculate min ask to satisfy flexible bids on the way?
+function submitAsk( _price, _volume, _sender) {
+    
+    var  ask = {};
+    ask.volume = _volume;
+    ask.id = this.idCounter++;
+    ask.price = _price;
+    ask.owner = _sender;
+    
+    console.log("Submitted ask: " + ask.id);
+
+
+    if(this.minAsk.id == 0) {
+        this.minAsk = ask;
+        return;
+    }
+    
+    
+    //Iterate over list till same price encountered
+    var curr = this.minAsk;
+    var prev = {};
+    prev.nex = this.minAsk;
+    while(curr && ask.price > curr.price) {
+        curr=n(curr);
+        prev = n(prev);
+    }
+    bind(prev, ask, curr);
+    
+} 
+
+function remove(prev, curr) {
+    prev.nex = curr.nex;
+    delete curr;
+}
+//TODO Magnus Has to be automatically called from the blockchain
+//Currently without accumulating, does accumulating make sense?
+function matching() {
+    
+    var prevBid = {id: 0};
+    var prevAsk = {id: 0};
+    var currBid = this.minBid;
+    var currAsk = this.minAsk;
+    var tmp;
+    
+    //Solve flexible bids first
+    var  askVolume = 0;
+    var  price = 0;
+    while(askVolume < this.flexBidVolume) {
+        askVolume += currAsk.volume;
+        price = currAsk.price;
+    }
+    currAsk = this.minAsk;
+    //Wouldnt it be fair that all of them go to the aftermarket
+    //instead of only the last one? Round-robin too much?
+    for(var i = 0; i < this.flexBids.length && currAsk; i++ ) {
+        if(currAsk.volume > this.flexBids[i].volume) {
+            this.matches.push({volume: this.flexBids[i].volume, price: price, askOwner: currAsk.owner, bidOwner: this.flexBids[i].owner});
+            currAsk.volume -= this.flexBids[i].volume;
+        }else if(currAsk.volume < this.flexBids[i].volume) {
+            this.matches.push({volume: currAsk.volume, price: price, askOwner: currAsk.owner, bidOwner: this.flexBids[i].owner});
+            this.flexBids[i].volume -= currAsk.volume;
+            tmp = currAsk.nex;
+            remove(prevAsk, currAsk);
+            currAsk = tmp; 
+            i-=1;
+        } else {
+            this.matches.push({volume: currAsk.volume, price: price, askOwner: currAsk.owner, bidOwner: this.flexBids[i].owner});
+            tmp = currAsk.nex;
+            remove(prevAsk, currAsk);
+            currAsk = tmp; 
+        }
+        console.log("Match: " + JSON.stringify(this.matches[this.matches.length-1]))
+    }
+    //Matching of bids and asks with fixed price
+    //Iterate till you come to the end of ask or bid lists
+    while(currAsk  && currBid) {
+
+        //Round robin so that everyone gets something?
+        if(currAsk.volume > currBid.volume) {
+            this.matches.push({volume: currBid.volume, price: currAsk.price, askOwner: currAsk.owner, bidOwner: currBid.owner});
+            currAsk.volume -= currBid.volume;
+        }else if(currAsk.volume < currBid.volume) {
+            this.matches.push({volume: currAsk.volume, price: price, askOwner: currAsk.owner, bidOwner: currBid.owner});
+            currBid.volume -= currAsk.volume;
+            tmp = currAsk.nex;
+            remove(prevAsk, currAsk);
+            currAsk = tmp; 
+        } else {
+            matches.push({volume: currAsk.volume, price: price, askOwner: currAsk.owner, bidOwner: this.flexBids[i].owner});
+            tmp = currAsk.nex;
+            remove(prevAsk, currAsk);
+            currAsk = tmp; 
+        }
+
+
+    }
+
+    //What remains remains...
+    
+}
+
+
+module.exports = {
+
+    submitFlexBid: submitFlexBid,
+    submitBid: submitBid,
+    submitAsk: submitAsk,
+    matching: matching,
+    minAsk: {id: 0, price: 0},
+    minBid: {id: 0, price: 0},
+    flexBidVolume: 0,
+    matches: [],
+    flexBids: [],
+    idCounter: 1
+
+}
+
+
+
---
+++ scripts/matching_algorithms/matching.js
@@ -0,0 +1,58 @@
+//Algorithm tests in javascript
+//you can write tests here, just require the defined algorithm, example already supplied
+
+var matching_alg = require('./acc_matching');
+
+matching_alg.submitBid(10, 20, 123445);
+matching_alg.submitAsk(10, 20, 123435);
+
+
+matching_alg.submitFlexBid(10, 20, 123)
+
+
+
+console.log("Printing flex bids: ");
+console.log(matching_alg.flexBids);
+
+
+NUM_TEST_BIDS = 1000;
+NUM_TEST_ASKS = 1000;
+NUM_TEST_FLEXBIDS = 1000;
+
+MAX_PRICE = 1000;
+MIN_PRICE = 10;
+
+MAX_VOLUME = 1000;
+MIN_VOLUME =  1;
+
+
+function randV() {
+    return Math.random() * (MAX_VOLUME - MIN_VOLUME) + MIN_VOLUME;
+}
+
+function randP() {
+    return Math.random() * (MAX_PRICE - MIN_PRICE) + MIN_PRICE;
+}
+
+
+var asks = [];
+var bids = [];
+var flexBids = [];
+var addressCounter = 0;
+
+
+//Generating test data
+for(var i = 0; i < NUM_TEST_ASKS; i++) {
+    matching_alg.submitAsk(randP(), randV(), addressCounter++); 
+}
+
+for(var i = 0; i < NUM_TEST_BIDS; i++) {
+    matching_alg.submitBid(randP(), randV(), addressCounter++); 
+}
+
+for(var i = 0; i < NUM_TEST_FLEXBIDS; i++) {
+    matching_alg.submitFlexBid(randV(), addressCounter++); 
+}
+
+
+matching_alg.matching();
