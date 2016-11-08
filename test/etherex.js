df0fdf5297e6cd5383f9cea6f14b2a05d58776c0
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-11-08 15:55:33 +0100

    Added testing interface for most of the functions

--- test/etherex.js
+++ test/etherex.js
@@ -1,7 +1,93 @@
 contract('Etherex', function(accounts) {
-  it("should assert true", function(done) {
+    
+
+  it("The contract should be deployed to the blockchain", function(done) {
+    //Initialize the contract
     var etherex = Etherex.deployed();
-    assert.isTrue(true);
+    assert.isTrue(etherex != undefined);
     done();
   });
+
+
+  describe("#submitBid", function() {
+
+    it("User should be able to submit", function(done){
+
+        done();
+
+    })
+
+  });
+
+
+  describe("#submitAsk", function() {
+
+    it("User should be able to submit", function(done){
+
+        done();
+
+    })
+
+  });
+
+
+describe("#submitReserveAsk", function() {
+
+    it("User should be able to submit", function(done){
+
+        done();
+
+    })
+
+  });
+
+
+describe("#matching", function() {
+
+    it("Matching should be done correctly (dependant on the algorithm)", function(done){
+
+        done();
+
+    })
+
+  });
+
+describe("#updateState", function() {
+
+    it("State should update correctly dependant on blocks", function(done) {
+
+        done();
+    
+
+    });
+
+
+})
+
+describe("#determineReservePrice", function() {
+
+    it("Correctness of determining the reserve price", function(done) {
+
+        done();
+    
+
+    });
+
+
+});
+
+describe("#settle", function() {
+
+    it("Correctness of settleing", function(done) {
+
+        done();
+    
+
+    });
+
+
+})
+
+
+
 });
